762c67be634c3eb4446f609046fa286a1f95f6cf
Filippo Baglioni <166933083+CiroBurro@users.noreply.github.com>
2025-07-05 18:47:23 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ makepkg -Csi --noconfirm
 cd "$HOME"
 
 #installo i pacchetti necessari
-sudo pacman -S --needed --noconfirm ufw zsh nvidia nvidia-utils lib32-nvidia-utils nvidia-settings vulkan-icd-loader lib32-vulkan-icd-loader lib32-mesa vulkan-intel lib32-vulkan-intel steam lutris code curl iwd impala discord ollama-cuda fastfetch linux-headers lm_sensors dmidecode python-pyqt6 python-yaml python-argcomplete python-darkdetect dkms openssl mokutil linux-headers
+sudo pacman -S --needed --noconfirm ufw zsh nvidia nvidia-utils lib32-nvidia-utils nvidia-settings vulkan-icd-loader lib32-vulkan-icd-loader lib32-mesa vulkan-intel lib32-vulkan-intel steam lutris code curl iwd impala discord ollama-cuda fastfetch linux-headers lm_sensors dmidecode python-pyqt6 python-yaml python-argcomplete python-darkdetect dkms openssl mokutil linux-headers uwsm libnewt
 
 #installo rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
@@ -25,6 +25,12 @@ sudo ./strap.sh
 #configuro la shell con oh my zsh
 chsh -s /usr/bin/zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+cat << 'EOF' >> ~/.zshrc
+if uwsm check may-start && uwsm select; then
+    exec uwsm start default
+fi
+EOF
 
 #configuro nvim
 [ -d "$HOME/.config/nvim" ] && mv "$HOME/.config/nvim" "$HOME/.config/nvim.bak_$(date +%s)"
