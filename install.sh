23482332f9b35a746d1150faf009ff583d5da18d
Filippo Baglioni <166933083+CiroBurro@users.noreply.github.com>
2025-07-05 18:08:20 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ makepkg -Csi --noconfirm
 cd "$HOME"
 
 #installo i pacchetti necessari
-sudo pacman -S --needed --noconfirm ufw zsh nvidia-open-dkms nvidia-utils lib32-nvidia-utils nvidia-settings vulkan-icd-loader lib32-vulkan-icd-loader lib32-mesa vulkan-intel lib32-vulkan-intel steam lutris code curl iwd impala discord ollama-cuda fastfetch linux-headers lm_sensors dmidecode python-pyqt6 python-yaml python-argcomplete python-darkdetect dkms openssl mokutil
+sudo pacman -S --needed --noconfirm ufw zsh nvidia nvidia-utils lib32-nvidia-utils nvidia-settings vulkan-icd-loader lib32-vulkan-icd-loader lib32-mesa vulkan-intel lib32-vulkan-intel steam lutris code curl iwd impala discord ollama-cuda fastfetch linux-headers lm_sensors dmidecode python-pyqt6 python-yaml python-argcomplete python-darkdetect dkms openssl mokutil linux-headers
 
 #installo rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
@@ -31,14 +31,13 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 git clone https://github.com/CiroBurro/nvim.git "$HOME/.config/nvim"
 
 #installo pacchetti necessari da aur
-yay -S --needed --noconfirm tabby brave lenovolegionlinux-dkms-git franz heroic-games-launcher spotify
+yay -S --needed --noconfirm tabby brave lenovolegionlinux-git franz heroic-games-launcher spotify
 
 #abilito i daemon e configuro il firewall
 sudo systemctl enable --now legiond ufw
+sudo systemctl disable sddm
 sudo ufw default allow outgoing
 sudo ufw default deny incoming
 sudo ufw allow ssh
 sudo ufw --force enable
 
-#scarico un modello llm
-ollama pull qwen3:8b
